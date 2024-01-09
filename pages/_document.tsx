f84f884b77b4e68b7776710f1a1387e98d0672d7
winbigcoms <bigcoms@imform.co.kr>
2024-01-09 18:04:01 +0900

    add og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,6 +34,21 @@ export default class MyDocument extends Document {
           <meta name='keyword' content='개인 블로그' />
           <meta property='og:locale' content='ko_KR' />
           <meta name='robots' content='index,follow' />
+          <meta property='og:url' content='https://www.white-win.dev/' />
+          <meta property='og:type' content='website' />
+          <meta property='og:description' content='안녕하세요. 백승일입니다.' />
+          <meta
+            property='og:image'
+            content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
+          />
+          <meta property='og:image:type' content='image/jpg' />
+          <meta property='twitter:domain' content='https://www.white-win.dev' />
+          <meta property='twitter:url' content='https://www.white-win.dev/' />
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta
+            name='twitter:image'
+            content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
+          />
         </Head>
         <body id='body'>
           <Main />
