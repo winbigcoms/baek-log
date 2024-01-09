f272f0601061fecaecf28a4e3ddaeaebb68888a1
bigcoms <bigcoms6290@gmail.com>
2024-01-10 01:36:04 +0900

    fix meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,26 +30,7 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang='ko'>
-        <Head>
-          <meta name='keyword' content='개인 블로그' />
-          <meta property='og:locale' content='ko_KR' />
-          <meta name='robots' content='index,follow' />
-          <meta property='og:url' content='https://www.white-win.dev/' />
-          <meta property='og:type' content='website' />
-          <meta property='og:description' content='안녕하세요. 백승일입니다.' />
-          <meta
-            property='og:image'
-            content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
-          />
-          <meta property='og:image:type' content='image/jpg' />
-          <meta property='twitter:domain' content='https://www.white-win.dev' />
-          <meta property='twitter:url' content='https://www.white-win.dev/' />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta
-            name='twitter:image'
-            content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
-          />
-        </Head>
+        <Head />
         <body id='body'>
           <Main />
           <div id='portal'></div>
--- pages/whoami.tsx
+++ pages/whoami.tsx
@@ -18,24 +18,6 @@ const WhoAmIPage = () => {
     <>
       <Head>
         <title>백승일 기술이력서</title>
-        <meta name='description' content='웹 개발자 백승일의 기술이력서' />
-        <meta name='keyword' content='개발자, 기술이력서, 포트폴리오, 프론트엔드' />
-        <meta property='og:url' content='https://www.white-win.dev/whoami' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='웹 개발자 백승일 기술이력서' />
-        <meta property='og:description' content='안녕하세요. 백승일입니다.' />
-        <meta
-          property='og:image'
-          content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
-        />
-        <meta property='og:image:type' content='image/jpg' />
-        <meta property='twitter:domain' content='https://www.white-win.dev' />
-        <meta property='twitter:url' content='https://www.white-win.dev/whoami' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta
-          name='twitter:image'
-          content='https://baek-log-img.s3.ap-northeast-2.amazonaws.com/og.png'
-        />
       </Head>
       <Wrapper className={'dark'} darkMode={true}>
         <FallingBg amount={30} />
