19e18ca8338667a6e9d4f32505c8b5d7f2c90ba8
bigcoms <bigcoms6290@gmail.com>
2024-03-11 20:03:23 +0900

    set aos

--- src/lib/registar.tsx
+++ src/lib/registar.tsx
@@ -2,11 +2,16 @@
 
 import { useServerInsertedHTML } from 'next/navigation';
 import { useStyledComponentsRegistry } from './styled-components';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function RootStyleRegistry({ children }: { children: React.ReactNode }) {
   const [StyledComponentsRegistry, styledComponentsFlushEffect] = useStyledComponentsRegistry();
 
   useServerInsertedHTML(() => <>{styledComponentsFlushEffect()}</>);
-
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
 }
