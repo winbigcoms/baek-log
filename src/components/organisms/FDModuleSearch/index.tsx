3d080e804bbf2a4fe5940661d99a08612bf7e02f
bigcoms <bigcoms6290@gmail.com>
2024-07-16 17:04:16 +0900

    모듈 조합

--- src/components/organisms/FDModuleSearch/index.tsx
+++ src/components/organisms/FDModuleSearch/index.tsx
@@ -1,10 +1,25 @@
 import { useRef, useState } from 'react';
 import { FirstDecendentModule } from 'src/types/firstDecendent';
+import styled from 'styled-components';
 
 interface FDModuleSearchProps {
   FDModule: FirstDecendentModule[];
   onSelectModule: (module: FirstDecendentModule) => void;
 }
+
+const FDModuleSearchStyle = styled.div`
+  input  {
+    width: 100%:
+    height: 30px;
+    text-indent: 0.5rem;
+  }
+  .result_module_container {
+    .module {
+      cursor: pointer;
+      margin-bottom: 3px;
+    }
+  }
+`;
 
 export const FDModuleSearch = (props: FDModuleSearchProps) => {
   const { FDModule, onSelectModule } = props;
@@ -17,24 +32,29 @@ export const FDModuleSearch = (props: FDModuleSearchProps) => {
     }
 
     timer.current = setTimeout(() => {
+      if (!text) return;
       const reg = new RegExp(`.*${text}.`, 'g');
       const searchedModule = FDModule.filter(module => reg.test(module.module_name));
       searchModule(searchedModule);
     }, 500);
   };
+
   return (
-    <div>
+    <FDModuleSearchStyle>
       <input type='text' onChange={e => onSearchModule(e.target.value)} />
-      {searchedModule.map(module => (
-        <div
-          key={module.module_id}
-          onClick={() => {
-            onSelectModule(module);
-          }}
-        >
-          {module.module_name}
-        </div>
-      ))}
-    </div>
+      <div className='result_module_container'>
+        {searchedModule.map(module => (
+          <div
+            key={module.module_id}
+            onClick={() => {
+              onSelectModule(module);
+            }}
+            className='module'
+          >
+            {module.module_name}({module.module_class})
+          </div>
+        ))}
+      </div>
+    </FDModuleSearchStyle>
   );
 };
--- src/components/templates/FirstDecendentModuleView/index.tsx
+++ src/components/templates/FirstDecendentModuleView/index.tsx
@@ -10,21 +10,46 @@ interface FirstDecendentModuleViewProps {
   moduleData: FirstDecendentModule[];
 }
 
+const Container = styled.div`
+  background-color: #1e1e1e;
+  color: #fff;
+
+  h2 {
+    padding: 10px;
+  }
+`;
+
 const FirstDecendentModuleViewStyle = styled.div`
   display: flex;
-  height: 100vh;
+  height: calc(100vh - 48px);
 
   & > div {
+    overflow-y: auto;
+
+    padding: 10px;
     flex: 1;
 
-    &:not(last-child) {
-      border-right: 1px solid #000;
+    &:not(:last-child) {
+      border-right: 1px solid #fff;
+    }
+
+    .section_title {
+      margin-bottom: 10px;
+
+      span {
+        margin-right: 5px;
+      }
     }
 
     .module_container {
-      padding: 10px;
+      border-bottom: 1px solid #000;
+      padding: 10px 0px;
+      &:first-child {
+        padding-top: 0px;
+      }
       .module_header {
         display: flex;
+        align-items: center;
         gap: 5px;
 
         .module_type_img {
@@ -39,10 +64,15 @@ const FirstDecendentModuleViewStyle = styled.div`
         .socket_upgrade {
           display: Flex;
           align-items: center;
-          input {
+
+          & > div {
+            margin-left: 5px;
             width: 1rem;
             height: 1rem;
-            border: 1px solid #000;
+            border: 1px solid #fff;
+            display: flex;
+            align-items: center;
+            justify-content: center;
           }
         }
       }
@@ -58,14 +88,21 @@ const FirstDecendentModuleViewStyle = styled.div`
           justify-content: center;
           width: 1.5rem;
           height: 1.5rem;
-          border: 1px solid #000;
+          border: 1px solid #fff;
           cursor: pointer;
         }
+
+        .module_level_text {
+          line-height: 1.5;
+        }
+      }
+
+      .remove_btn {
+        text-align: right;
       }
     }
 
     &.module_effects {
-      padding: 10px;
       & > div {
         margin-bottom: 10px;
       }
@@ -76,10 +113,16 @@ const FirstDecendentModuleViewStyle = styled.div`
 export const FirstDecendentModuleView = (props: FirstDecendentModuleViewProps) => {
   const { moduleData } = props;
   const [selectedModule, selectModule] = useState<SelectedFirstDecendentModule[]>([]);
-  const [socketMax] = useState(40);
 
   const onSelectModule = (module: FirstDecendentModule) => {
+    const isAlreadySelectModule = selectedModule.find(data => data.module_id === module.module_id);
+
+    if (isAlreadySelectModule) return;
     selectModule(state => [...state, { ...module, level: 0, socketUpgrade: false }]);
+  };
+
+  const onRemoveModule = (moduleId: string) => {
+    selectModule(state => state.filter(data => data.module_id !== moduleId));
   };
 
   const onControlModuleLevel = (moduleId: string, option: -1 | 1) => {
@@ -99,75 +142,87 @@ export const FirstDecendentModuleView = (props: FirstDecendentModuleViewProps) =
       })
     );
   };
-  console.log(selectedModule);
 
   return (
-    <FirstDecendentModuleViewStyle>
-      <FDModuleSearch FDModule={moduleData} onSelectModule={onSelectModule} />
-      <div>
+    <Container>
+      <h2>first decendent module maker</h2>
+      <FirstDecendentModuleViewStyle>
         <div>
-          {selectedModule.reduce((acc, cur) => {
-            return acc + cur.module_stat[cur.level].module_capacity;
-          }, 0)}
-          /{socketMax}
+          <div className='section_title'>검색</div>
+          <FDModuleSearch FDModule={moduleData} onSelectModule={onSelectModule} />
         </div>
         <div>
-          {selectedModule.map(data => (
-            <div key={data.module_id} className='module_container'>
-              <div className='module_header'>
-                <div className='module_type_img'>
-                  <Image fill src={data.image_url} alt='모듈이미지' />
+          <div className='section_title'>
+            <span> 수용량</span>
+            {selectedModule.reduce((acc, cur) => {
+              const socketAmount = cur.module_stat[cur.level].module_capacity;
+              const amount = cur.socketUpgrade ? Math.floor(socketAmount / 2) : socketAmount;
+              return acc + amount;
+            }, 0)}
+          </div>
+          <div>
+            {selectedModule.map(data => (
+              <div key={data.module_id} className='module_container'>
+                <div className='module_header'>
+                  <div className='module_type_img'>
+                    <Image fill src={data.image_url} alt='모듈이미지' />
+                  </div>
+                  <div>
+                    <div>
+                      {data.module_name} / {data.module_class}
+                    </div>
+                    <div>{data.module_socket_type}</div>
+                    <label className='socket_upgrade'>
+                      소켓 강화
+                      <div onClick={() => onControlModuleSocketUpgrade(data.module_id)}>
+                        {data.socketUpgrade && <span>v</span>}
+                      </div>
+                    </label>
+                  </div>
                 </div>
-                <div>
-                  <div>{data.module_name}</div>
-                  <label className='socket_upgrade'>
-                    소켓 강화
-                    <input
-                      type='checkbox'
-                      checked={data.socketUpgrade}
-                      onChange={() => {
-                        onControlModuleSocketUpgrade(data.module_id);
+                <div className='module_body'>
+                  <div className='module_level'>
+                    <div
+                      role='button'
+                      className='module_level_btn'
+                      onClick={() => {
+                        if (data.level === 0) return;
+                        onControlModuleLevel(data.module_id, -1);
                       }}
-                    />
-                  </label>
-                </div>
-              </div>
-              <div className='module_body'>
-                <div className='module_level'>
-                  <div
-                    role='button'
-                    className='module_level_btn'
-                    onClick={() => {
-                      if (data.level === 0) return;
-                      onControlModuleLevel(data.module_id, -1);
-                    }}
-                  >
-                    -
+                    >
+                      -
+                    </div>
+                    <div className='module_level_text'>레벨: {data.level}</div>
+                    <div
+                      role='button'
+                      className='module_level_btn'
+                      onClick={() => {
+                        if (data.level === data.module_stat.length - 1) return;
+                        onControlModuleLevel(data.module_id, 1);
+                      }}
+                    >
+                      +
+                    </div>
                   </div>
-                  <div>레벨: {data.level}</div>
-                  <div
-                    role='button'
-                    className='module_level_btn'
-                    onClick={() => {
-                      if (data.level === data.module_stat.length - 1) return;
-                      onControlModuleLevel(data.module_id, 1);
-                    }}
-                  >
-                    +
+                  <div>소켓: {data.module_stat[data.level].module_capacity}</div>
+                  <div>옵션: {data.module_stat[data.level].value}</div>
+                  <div className='remove_btn' onClick={() => onRemoveModule(data.module_id)}>
+                    제거
                   </div>
                 </div>
-                <div>소켓: {data.module_stat[data.level].module_capacity}</div>
-                <div>옵션: {data.module_stat[data.level].value}</div>
               </div>
+            ))}
+          </div>
+        </div>
+        <div className='module_effects'>
+          <div className='section_title'>최종 옵션</div>
+          {selectedModule.map(module => (
+            <div className='options' key={module.module_id}>
+              {module.module_stat[module.level].value}
             </div>
           ))}
         </div>
-      </div>
-      <div className='module_effects'>
-        {selectedModule.map(module => (
-          <div key={module.module_id}>{module.module_stat[module.level].value}</div>
-        ))}
-      </div>
-    </FirstDecendentModuleViewStyle>
+      </FirstDecendentModuleViewStyle>
+    </Container>
   );
 };
